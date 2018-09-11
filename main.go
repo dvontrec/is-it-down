72ca2af4d3dc625da70e6ea3d723a4871628d63d
Dvontre Coleman <dvontrec@gmail.com>
2018-09-11 05:57:22 -0400

    Setup skeleton of the program

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://twitter.com",
+		"http://amazon.com",
+		"http://golang.org",
+	}
+}
