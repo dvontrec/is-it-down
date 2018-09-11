e91591939b2133b1a7b5172cb158983484c297f2
Dvontre Coleman <dvontrec@gmail.com>
2018-09-11 17:47:17 -0400

    Update code to use channels and routines

--- main.go
+++ main.go
@@ -16,22 +16,32 @@ func main() {
 		"http://golang.org",
 	}
 
-	for _, link := range links {
-		checkLink(link)
-	}
+	// Creates a new channel to communicate with new go routines
+	c := make(chan string)
 
+	for _, link := range links {
+		// Sets up a new go routine to call the checkline function
+		go checkLink(link, c)
+	}
+	// Loops through the number of links to check for a response from the channels
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
 }
 
 // Function to check if the link is responding to http traffic
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	// If there is an error
 	if err != nil {
 		// Print that the link may be down
 		fmt.Println(link, "Might be down")
+		// Sends message to channel
+		c <- "Might be down I think"
 		// return
 		return
 	}
 	// print that the link os up
 	fmt.Println(link, "Is up")
+	c <- "Yup, its up"
 }
