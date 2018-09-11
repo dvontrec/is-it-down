8c8a1ca1716703f3046ec8ef8d2b404c29131c3b
Dvontre Coleman <dvontrec@gmail.com>
2018-09-11 06:17:15 -0400

    Setup basic functionality

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
+	// creates a slice of links
 	links := []string{
 		"http://google.com",
 		"http://facebook.com",
@@ -9,4 +15,23 @@ func main() {
 		"http://amazon.com",
 		"http://golang.org",
 	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+
+}
+
+// Function to check if the link is responding to http traffic
+func checkLink(link string) {
+	_, err := http.Get(link)
+	// If there is an error
+	if err != nil {
+		// Print that the link may be down
+		fmt.Println(link, "Might be down")
+		// return
+		return
+	}
+	// print that the link os up
+	fmt.Println(link, "Is up")
 }
